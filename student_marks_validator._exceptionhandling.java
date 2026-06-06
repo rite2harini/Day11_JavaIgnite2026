e429ffd57baa99cc0e0f4c95b57baf1f797b2bb1
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-06 16:04:58 +0530

    Update student_marks_validator._exceptionhandling.java

--- student_marks_validator._exceptionhandling.java
+++ student_marks_validator._exceptionhandling.java
@@ -1,12 +1,25 @@
-/*
-Create a program that:
+public class MarksValidation {
+	static void checkMarks(int marks) throws InvalidMarksException {
+        if (marks < 0 || marks > 100) {
+            throw new InvalidMarksException("Invalid Marks");
+        } else {
+            System.out.println("Valid Marks");
+        }
+    }
 
-Takes marks of a student
-Rules:
-If marks < 0 OR marks > 100 → throw custom exception InvalidMarksException
-Requirements:
-Use throw inside method
-Use throws in method signature
-Output:
-Valid Marks OR InvalidMarksException handled
-*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter marks: ");
+            int marks = sc.nextInt();
+
+            checkMarks(marks);
+
+        } catch (InvalidMarksException e) {
+            System.out.println("InvalidMarksException handled");
+        }
+    }
+}
+
+
