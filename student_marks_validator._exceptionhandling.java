fbe28b85139f633ffa241bc4bf7b50861171f8a8
ece25becd86-NANDINIBISWAPRIYA <ece.25becd86@silicon.ac.in>
2026-06-06 21:38:09 +0530

    Update student_marks_validator._exceptionhandling.java

--- student_marks_validator._exceptionhandling.java
+++ student_marks_validator._exceptionhandling.java
@@ -10,3 +10,51 @@ Use throws in method signature
 Output:
 Valid Marks OR InvalidMarksException handled
 */
+
+package JavaIgniteDay11;
+import java.util.Scanner;
+
+public class Students_Mark_Validator {
+	class InvalidMarksException extends Exception {
+	    public InvalidMarksException(String message) {
+	        super(message);
+	    }
+	}
+
+	public class Student_Marks_Validator {
+
+	    static void checkMarks(int marks) throws InvalidMarksException 
+	    {
+	        if (marks < 0 || marks > 100) 
+	        {
+	            throw new InvalidMarksException("Marks must be between 0 and 100");
+	        }
+
+	        System.out.println("Valid Marks");
+	    }
+
+	    public static void main(String[] args) {
+	    	// TODO Auto-generated method stub
+	    	
+	        Scanner sc = new Scanner(System.in);
+	        try
+	        {
+	            System.out.print("Enter marks: ");
+	            int marks = sc.nextInt();
+
+	            checkMarks(marks);
+	        }
+	        catch (InvalidMarksException e) {
+	            System.out.println("InvalidMarksException handled");
+	            System.out.println(e.getMessage());
+	        }
+	        catch (Exception e) {
+	            System.out.println("Invalid Input");
+	        }
+
+	        sc.close();
+	    }
+	}
+
+}
+
