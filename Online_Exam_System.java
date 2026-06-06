717a5a46a8e64a939ca21a2b95d1199739e109da
25BCSA28-SATYAJIT <satyajit9090xa@gmail.com>
2026-06-06 16:44:58 +0530

    Update Online_Exam_System.java

--- Online_Exam_System.java
+++ Online_Exam_System.java
@@ -10,3 +10,45 @@ If student ID is null → NullPointerException handling
 Twist:
 Use nested try-catch blocks
 */
+package java_internship_day11;
+import java.util.Scanner;
+class InvalidUsernameException extends Exception {
+    InvalidUsernameException(){
+        super("Username cannot be empty");
+    }
+}
+class WeakPasswordException extends Exception {
+	WeakPasswordException(){
+		super("Password is weak ");
+	}
+}
+class SnartLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String username,pass;
+		try {
+			System.out.println("Enter the username for login :");
+			username = sc.nextLine();
+			if(username.isEmpty())
+				throw new InvalidUsernameException();
+
+			System.out.println("Enter the password");
+			pass=sc.nextLine();
+			if(pass.length() < 6) {
+				throw new WeakPasswordException();
+			}
+			
+			
+		}
+		catch(WeakPasswordException e) {
+			System.out.println(e.getMessage());
+		}
+		catch(InvalidUsernameException e) {
+			System.out.println(e.getMessage());
+		}
+		sc.close();
+	}
+}
