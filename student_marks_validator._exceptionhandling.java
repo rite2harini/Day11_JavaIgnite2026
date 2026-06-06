7bafbe4e247a65fc3f905906028ad5476bcde939
IpshitaDutta25BCSA54 <ipshitadutta28sept@gmail.com>
2026-06-06 15:17:57 +0530

    Update student_marks_validator._exceptionhandling.java

--- student_marks_validator._exceptionhandling.java
+++ student_marks_validator._exceptionhandling.java
@@ -1,12 +1,34 @@
-/*
-Create a program that:
+import java.util.Scanner;
 
-Takes marks of a student
-Rules:
-If marks < 0 OR marks > 100 → throw custom exception InvalidMarksException
-Requirements:
-Use throw inside method
-Use throws in method signature
-Output:
-Valid Marks OR InvalidMarksException handled
-*/
+class InvalidMarksException extends Exception {
+    InvalidMarksException(String msg) {
+        super(msg);
+    }
+}
+
+public class student_marks_validator_exceptionhandling {
+
+    static void validateMarks(int marks) {
+        try {
+            if (marks < 0 || marks > 100) {
+                throw new InvalidMarksException("InvalidMarksException handled");
+            }
+
+            System.out.println("Valid Marks");
+
+        } catch (InvalidMarksException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Marks: ");
+        int marks = sc.nextInt();
+
+        validateMarks(marks);
+
+        sc.close();
+    }
+}
