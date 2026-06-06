5d4d2fdc32ec148fab6c9f4bab66f9f7c5731f5a
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-07 00:25:06 +0530

    Implement custom exceptions for patient priority validation
    Added custom exception handling for age and emergency level validation.

--- Hospital_Emergency_System.java
+++ Hospital_Emergency_System.java
@@ -11,3 +11,62 @@ Emergency level not between 1–5 → InvalidPriorityException
 Output:
 Priority Level assigned OR Exception message
 */
+import java.util.Scanner;
+class InvalidAgeException extends Exception
+{
+    public InvalidAgeException(String msg)
+    {
+        super(msg);
+    }
+}
+class InvalidPriorityException extends Exception
+{
+    public InvalidPriorityException(String msg)
+    {
+        super(msg);
+    }
+}
+public class priority_to_patients 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        int age , emergency_level;
+        System.out.print("Enter Patient Name : ");
+        name = sc.nextLine();
+        System.out.print("Enter Age : ");
+        age = sc.nextInt();
+        System.out.print("Enter Emergency level : ");
+        emergency_level = sc.nextInt();
+        try
+        {
+            if(age < 0 || age > 120)
+            {
+                throw new InvalidAgeException("Invalid Age !!!! ");
+            }
+            if(emergency_level < 1 || emergency_level >5)
+            {
+                throw new InvalidPriorityException("Invalid Emergency Level !!!!");
+
+            }
+            else
+            {
+                System.out.println("Priority Level Assigned");
+            }
+        }
+        catch(InvalidAgeException e)
+        {
+            System.out.println("Invalid Age !!!!");
+        }
+        catch(InvalidPriorityException e)
+        {
+            System.out.println("Invalid Emergency Level !!!!");
+        }
+        
+
+
+
+    }
+    
+}
