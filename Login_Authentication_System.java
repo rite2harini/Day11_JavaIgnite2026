ded41b369c7bee0c0c7fcc8624490a88380b9319
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-07 20:51:44 +0530

    Update Login_Authentication_System.java

--- Login_Authentication_System.java
+++ Login_Authentication_System.java
@@ -1,8 +1,42 @@
-/*
-Create a login system:
+package java_internship_day11;
+import java.util.Scanner;
+class InvalidUsernameException extends Exception {
+    InvalidUsernameException(){
+        super("Username cannot be empty");
+    }
+}
+class WeakPasswordException extends Exception {
+	WeakPasswordException(){
+		super("Weak password");
+	}
+}
+class SnartLogin {
 
-Input username and password
-Rules:
-Username cannot be empty → throw InvalidUsernameException
-Password must be at least 6 characters → throw WeakPasswordException
-*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String user,password;
+		try {
+			System.out.println("Enter the username for login :");
+			user = sc.nextLine();
+			if(user.isEmpty())
+				throw new InvalidUsernameException();
+
+			System.out.println("Enter the password");
+			password=sc.nextLine();
+			if(password.length() < 6) {
+				throw new WeakPasswordException();
+			}
+			
+			
+		}
+		catch(WeakPasswordException e) {
+			System.out.println(e.getMessage());
+		}
+		catch(InvalidUsernameException e) {
+			System.out.println(e.getMessage());
+		}
+		sc.close();
+	}
+}
