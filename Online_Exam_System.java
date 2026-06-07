14762ea889bf23ef3220369dbafda80aef13d977
25BEEG04 <abhijitbiswal700@gmail.com>
2026-06-07 21:20:36 +0530

    Update Online_Exam_System.java

--- Online_Exam_System.java
+++ Online_Exam_System.java
@@ -1,12 +1,50 @@
-/*
-Create a program that:
-
-Takes student answers score
-Calculates percentage
-Rules:
-Marks cannot exceed 100 → InvalidMarksException
-Negative marks not allowed → NegativeMarksException
-If student ID is null → NullPointerException handling
-Twist:
-Use nested try-catch blocks
-*/
+package JavaIgniteDay11;
+import java.util.Scanner;
+	class InvalidMarksException extends Exception {
+	    public InvalidMarksException(String message){
+	        super(message);
+	    }
+	}
+	class NegativeMarksException extends Exception{
+	    public NegativeMarksException(String message) {
+	        super(message);
+	    }
+	}
+	public class Online_Exam {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        try  {
+	            System.out.print("Enter Student ID: ");
+	            String studentId = sc.nextLine();
+	            try  {
+	                if (studentId.isEmpty()) {
+	                    studentId = null;
+	                }
+	                System.out.print("Enter Marks out of 100: ");
+	                double marks = sc.nextDouble();
+	                if (marks < 0) {
+	                    throw new NegativeMarksException("Negative marking not allowed");
+	                }
+	                if (marks > 100) {
+	                    throw new InvalidMarksException( "Marks can't exceed 100");
+	                }
+	                double percentage = marks; 
+	                System.out.println("Student ID: " + studentId);
+	                System.out.println("Percentage: " + percentage + "%");
+              }
+	            catch (NegativeMarksException e){
+	                System.out.println(e.getMessage());
+	            }
+	            catch (InvalidMarksException e) {
+	                System.out.println(e.getMessage());
+	            }
+	            catch (NullPointerException e){
+	                System.out.println("Student ID can't be empty");
+	            }
+	        }
+	        catch (Exception e) {
+	            System.out.println("Invalid Input");
+	        }
+	        sc.close();
+	    }
+  }
