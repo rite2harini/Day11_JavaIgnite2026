6289aab37aa4eb5880faffe1d827864470ca6d71
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-09 16:55:44 +0530

    Add exception handling for age and priority inputs

--- Hospital_Emergency_System.java
+++ Hospital_Emergency_System.java
@@ -11,3 +11,61 @@ Emergency level not between 1–5 → InvalidPriorityException
 Output:
 Priority Level assigned OR Exception message
 */
+package JAVA_DAY_11;
+import java.util.Scanner;
+class InvalidAgeException extends Exception
+{
+	
+	private static final long serialVersionUID = 1L;
+
+	public InvalidAgeException(String msg)
+	{
+		super(msg);
+	}
+}
+class InvalidPriorityException extends Exception
+{
+	
+	private static final long serialVersionUID = 1L;
+
+	public InvalidPriorityException(String msg)
+	{
+		super(msg);
+	}
+}
+public class HOSPITAL_EMERGENCY_SYSTEM {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int age;
+		int priority;
+		String name;
+		
+		try {
+			System.out.println("Enter the name of the patient:");
+			name = sc.nextLine();
+			System.out.println("Enter the age:");
+			age = sc.nextInt();
+			if(age<0 || age>120)
+			{
+				throw new InvalidAgeException("Invalid Age!\nTry again!");
+			}
+			System.out.println("Enter the priority level (1-5):");
+			priority = sc.nextInt();
+			if(priority<1 || priority>5)
+			{
+				throw new InvalidPriorityException("Invalid priority!\nTry again!");
+			}
+			
+			System.out.println("Priority level assigned! Patient "+name+" has been registered!");
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		sc.close();
+	}
+
+}
+
